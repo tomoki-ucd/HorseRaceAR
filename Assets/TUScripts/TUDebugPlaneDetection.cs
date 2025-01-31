9966d5e1199f1c04067682947d19fcbac8cc950e
tomoki-ucd <tomoki.ucd@gmail.com>
2025-01-31 20:03:54 +0900

    Moved ARPlane detecting behavior from Update() to OnPlanesChanged().

--- Assets/TUScripts/TUDebugPlaneDetection.cs
+++ Assets/TUScripts/TUDebugPlaneDetection.cs
@@ -1,7 +1,8 @@
 #nullable enable
 
-using UnityEngine;
+using UnityEngine;  // Defines Touch and Input
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;  // Defins TrackableType
 using System.Collections.Generic;
 
 public class TUDebugPlaneDetection : MonoBehaviour
@@ -9,34 +10,11 @@ public class TUDebugPlaneDetection : MonoBehaviour
     private ARPlaneManager? planeManager;
     private ARPlane? lockedPlane = null;    // Nullable
 
-    // The method to stop detecting new planes
-    private void OnPlanesChanged(ARPlanesChangedEventArgs args)
-    {
-        if(lockedPlane != null) return;
-
-//        if(args.added != null && args.added.Count > 0)  // If there are newly detected planes
-//        {
-            foreach(var plane in args.added)
-            {
-                if(lockedPlane == null)
-                {
-                    lockedPlane = plane;
-                }
-                else
-                {
-                    plane.gameObject.SetActive(false);
-                }
-            }
-
-            if (lockedPlane != null)
-            {
-                planeManager.enabled = false;
-            }
-//        }
-
-        // This condition check seems a redundant, however, it's defensive programming practice.
-        // args.added =! seems to make sure that args.added.Count > 0, however, there could be unexpected situation due to a bug.
-    }
+    [SerializeField]
+    private ARRaycastManager raycastManager;    // Where is ARRaycastManager in this scene?
+                                                // --> It is usually set to XROrigin
+    private List<ARRaycastHit> raycastHits = new List<ARRaycastHit>();  // Why don't it use var declaration?
+                                                                        // `var` keyword is only avaialble in local variables.
 
     // Start is called before the first frame update
     void Start()
@@ -50,11 +28,23 @@ public class TUDebugPlaneDetection : MonoBehaviour
             return;
         }
 
+        // Get ARRaycastManager
+        raycastManager = FindObjectOfType<ARRaycastManager>();
+
+        // Check ARRaycastManager
+        if(raycastManager != null)
+            Debug.Log("ARRaycastManager successfully asssigned.") ;
+        else
+            Debug.Log("ARCastManager is still null.");
+
+
         // Subscribe to plane detection event
         planeManager.planesChanged += OnPlanesChanged;
-        
+
     }
 
+
+    // The default of OnDestroy() method is empty.
     void OnDestroy()
     {
         if (planeManager != null)
@@ -63,33 +53,82 @@ public class TUDebugPlaneDetection : MonoBehaviour
         }
     }
 
+
     // Update is called once per frame
     void Update()
     {
-        // Log detected planes
-        //for (int i = 0; i < planeManager.trackables.Length; i++)    // This does not work because TrackableCollection<T> does not support indexing
-                                                                    // You need to convert TrackableCollection to List
-        // var planes = new List<ARPlane>(planeManager.trackables); // Convert TrackableCollection to List
-                                                                 // List<T> is included in System.Collections.Genericarg
-                                                                 // But After all this does not work because TrakableCollections<T> is not convertable to IEnumerable<T>
-        // for (int i = 0; i < planes.Count; i++)    // .Length is for Array, .Count is for List
-                                                  // Array is fixed size, List is adjustable size
-        if(lockedPlane == null)
+        // Check for touch input
+        if(Input.touchCount > 0)    // Input.touchCount should be cummulative, right?
         {
-            foreach (var plane in planeManager.trackables)
+            Touch touch = Input.GetTouch(0);    // Touch is strcut defined in UnityEngine namespace.
+                                                // GetTouch(0) means the first touch.
+
+            if (touch.phase == TouchPhase.Began)    //TouchPhase is a enum.
             {
-                // Check if planes have been detected
-    //            if(plane != null)   // This line is unnecessary as plane will never be null
-    //            {
-                    // ARPlane plane = planes[i];
-                Debug.Log($"Plane detected: {plane.trackableId}, Center: {plane.center}");  // Be carefule that "d" of "Id" is lowercase
-    //            }
-    //            else    // This "else" will never be executed as plane is always non-null
-    //            {
-    //                Debug.Log("Planes are not detected yet");
-    //            }
+                Debug.Log($"touch.phase == TouchPhase.Began --> true ");
+
+                // Perform a raycast from the screen point
+                // bool Raycast(Vector2 screenPoint, List<ARRaycastHit> hitResults, TrackableType that raycast should interact with)
+                // Raycast() returns true when it successfully hits one or more trackables.
+                Debug.Log($"touch.position : {touch.position}");
+                if(raycastManager.Raycast(touch.position, raycastHits, TrackableType.PlaneWithinPolygon))
+                {
+                    Debug.Log($"Raycast was emitted and hit {raycastHits.Count} objects."); // Count is property.
+                    // Get the fist hit
+                    var hit = raycastHits[0];
+
+                    // Get the corresponding ARPlane
+                    ARPlane hitPlane = planeManager.GetPlane(hit.trackableId);  // hit has trackableId of hit object?
+
+                    if(hitPlane != null)
+                    {
+                        Debug.Log($"Hit plane detected: {hitPlane.trackableId}");
+                        KeepOnlyThisPlane(hitPlane);
+                    }
+                    else
+                    {
+                        Debug.Log("No corresponding plane found for the raycast hit.");
+                    }
+                }
+                else{
+                    Debug.Log("Raycast did not hit any planes.");
+                }
+                Debug.Log("touch.phase == TouchPhase.Began --> false");
             }
         }
-        Debug.Log($"Plane has been locked. LockedPlane : {lockedPlane.trackableId}");
+    }
+
+
+    private void KeepOnlyThisPlane(ARPlane planeToKeep)
+    {
+        lockedPlane = planeToKeep;
+
+        // Disable all other planes
+        foreach (ARPlane plane in planeManager.trackables)
+        {
+            if (plane != planeToKeep)
+            {
+                plane.gameObject.SetActive(false);
+            }
+        }
+
+        Debug.Log($"Kept plane: {planeToKeep.trackableId}");
+
+        if (lockedPlane != null)
+        {
+            planeManager.enabled = false;
+        }
+    }
+
+
+    // The method to stop detecting new planes
+    private void OnPlanesChanged(ARPlanesChangedEventArgs args)
+    {
+        // Immediately return if the lockedPlane already exists
+        if(lockedPlane != null) return;
+
+        Debug.Log($"planeManager.trackables.count : {planeManager.trackables.count}");
+        if(planeManager.trackables.count == 0)
+            Debug.Log("No planes detected by ARPlaneManager.");
     }
 }

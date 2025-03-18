24b594d0c0e883849e3459edde484b8c5788d2be
tomoki-ucd <tomoki.ucd@gmail.com>
2025-03-19 00:35:49 +0900

    Clean the code of HorseNameGenerator.

--- Assets/Scripts/MainMenu/HorseNameGenerator.cs
+++ Assets/Scripts/MainMenu/HorseNameGenerator.cs
@@ -1,22 +1,18 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 
+/// <summary>
+/// Provides the functionality to generate horse names using generative AI.
+/// </summary>
 public class HorseNameGenerator : MonoBehaviour
 {
     // Instant fields
-    private readonly HttpClient _httpClient = new HttpClient();
     // TO DO : Encrypt the API key.
     private const string XAI_API_KEY = "";
     private const string BASE_URL = "https://api.x.ai/v1";  // grok url
     [SerializeField] private Button _startToPlayButton;
-//    private bool isRunning = false;
-
 
 
     // Start is called before the first frame update
@@ -27,39 +23,17 @@ public class HorseNameGenerator : MonoBehaviour
         if(_startToPlayButton != null)
         {
             // TO DO: Consider using UnitTask library
-            CustomLogger.Print(this, "Add RequestHorseName()");
-//            _startToPlayButton.onClick.AddListener(StartSendRequest);
             _startToPlayButton.onClick.AddListener(() => {StartCoroutine(SendRequest());});
-
-//            _startToPlayButton.onClick.AddListener(async () => 
-//            {
-//                if(!isRunning)
-//                {
-//                    isRunning = true;
-//                    StartCoroutine(SendRequest());
-//                    isRunning = false;
-//                }
-//            });
         }
     }
 
-//    public async Task<string> RequestHorseNames()
-//    {
-//        HttpResponseMessage response = await _httpClient.GetAsync(url);
-//        response.EnsureSuccessStatusCode();
-//        string responseString = await response.Content.ReadAsStringAsync();
-//        CustomLogger.Print(this, responseString);
-//        return responseString;
-//    }
 
-    private void StartSendRequest()
-    {
-        StartCoroutine(SendRequest());
-    }
-
+    /// <summary>
+    /// Coroutine to send HTTP request to grok.
+    /// </summary>
+    /// <returns> IEnumerator </returns>
     IEnumerator SendRequest()
     {
-        CustomLogger.Print(this, $"StartToPlayButton is clicked.");
         string url = BASE_URL + "/models";
         string apiKey = XAI_API_KEY;
         UnityWebRequest request = UnityWebRequest.Get(url);

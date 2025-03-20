4da7e76cf3fcbe00d879f96a7d339abc5c14714f
tomoki-ucd <tomoki.ucd@gmail.com>
2025-03-21 00:04:32 +0900

    Restore SelectHorse.cs that was deleted mistakenly.

---
+++ Assets/Scripts/SelectHorse/HorseSelector.cs
@@ -0,0 +1,107 @@
+using Newtonsoft.Json;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;    // For TextMeshProUGUI type
+
+/// <summary>
+/// Provides the functionality to select a horse to bet.
+/// </summary>
+public class HorseSelector : MonoBehaviour
+{
+    // Instant fields
+    private string _selectedHorse;
+    private string[] _horseNames = new string[Horse.NUM_OF_HORSES];
+    private GameObject _selectedButtonObj = null;
+    [SerializeField] private Button[] _selectHorseButtons = new Button[Horse.NUM_OF_HORSES];
+    [SerializeField] private Button _fixSelectButton;
+//    [SerializeField] private TextMeshProUGUI testText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeHorseButtonName();
+        AddListeners();
+    }
+
+
+    /// <summary>
+    /// Initialize the horse buttons' names with the names acquired by grok.
+    /// </summary>
+    private void InitializeHorseButtonName()
+    {
+        _horseNames = JsonConvert.DeserializeObject<string[]>(AppData.HorseName);
+        for(int i = 0; i < _horseNames.Length; i++)
+        {
+            if(_selectHorseButtons[i] == null)
+            {
+                Debug.Log("_selectHorseButtons is null!!");
+            }
+            _selectHorseButtons[i].transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = _horseNames[i];
+        }
+    }
+
+
+    /// <summary>
+    /// Add event listeners.
+    /// </summary>
+    private void AddListeners()
+    {
+        // Listeners for each horse name button.
+        foreach(Button button in _selectHorseButtons)
+        {
+            if(button == null)
+            {
+                CustomLogger.Print(this, $"{button} is null.");
+                continue;
+            }
+            button.onClick.AddListener(() => SelectHorse(button.gameObject));
+        }
+
+        // Listeners for "Select" button.
+        _fixSelectButton.onClick.AddListener(OnClicked);
+    }
+
+
+    /// <summary>
+    /// Select a horse to bet.
+    /// </summary>
+    /// <param name="buttonObj"></param>/// 
+    private void SelectHorse(GameObject buttonObj)
+    {
+        if(_selectedButtonObj == null)  // If there was no selected button
+        {
+            SetButtonOutlineActive(buttonObj, true);    // Display the outline for the selected button
+            SetFixSelectButtonActive();                 // Activate "Select" button
+        }
+        else if(_selectedButtonObj != buttonObj)    // If the clicked button is not the one already selected
+        {
+            SetButtonOutlineActive(buttonObj, true);
+            SetButtonOutlineActive(_selectedButtonObj, false);
+        }
+        _selectedButtonObj = buttonObj;
+    }
+
+
+    /// <summary>
+    /// Activate/Deactivate the Button Outline.
+    /// </summary>
+    /// <param name="button"></_param>/// 
+    /// <param name="isActive"></_param>/// 
+    private void SetButtonOutlineActive(GameObject button, bool isActive)
+    {
+        Transform outline = button.transform.Find("ButtonOutline");
+        outline.gameObject.SetActive(isActive);
+    }
+
+    private void SetFixSelectButtonActive()
+    {
+        _fixSelectButton.interactable = true;
+    }
+
+
+    private void OnClicked()
+    {
+        SceneManager.LoadScene("Race");
+    }
+}
